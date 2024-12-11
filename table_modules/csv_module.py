5977b6e7c1747e94648eba593cb89c3adec19d66
Ivan Nikolaev <149399445+0bobux@users.noreply.github.com>
2024-12-11 20:04:28 +0300

    добавил документацию в csv

--- table_modules/csv_module.py
+++ table_modules/csv_module.py
@@ -11,17 +11,20 @@ def load_table(file_path, auto_detect_types=False):
 
     Returns:
         dict: Таблица с ключами 'header' и 'rows'.
+
+    Raises:
+        ValueError: Если возникает ошибка при загрузке файла.
     """
     try:
         with open(file_path, mode="r", newline="", encoding="utf-8") as file:
-            reader = csv.reader(file)
-            header = next(reader)
-            rows = [row for row in reader]
+            reader = csv.reader(file) # reader для чтения строк
+            header = next(reader) # читаем 1 строку как заголовок
+            rows = [row for row in reader] # список списков каждый внутренний список представляет одну строку из CSV
 
-        table = {"header": header, "rows": rows}
+        table = {"header": header, "rows": rows} # внутреннее представление таблицы
 
-        if auto_detect_types:
-            table['column_types'] = auto_detect_column_types(table)
+        if auto_detect_types: # если параметр auto_detect_types=True
+            table['column_types'] = auto_detect_column_types(table) # функция создает новый ключ и возвращает вложеный словарь с определёнными типами данных для каждого столбца
 
         return table
     except Exception as e:
@@ -30,12 +33,22 @@ def load_table(file_path, auto_detect_types=False):
 def save_table(table, file_path):
     """
     Сохраняет таблицу в CSV-файл.
+
+    Args:
+        table (dict): Таблица в виде словаря с ключами header и rows.
+        file_path (str): Путь, куда нужно сохранить файл.
+
+    Returns:
+        None: Функция ничего не возвращает.
+
+    Raises:
+        ValueError: Если возникает ошибка при сохранении файла.
     """
     try:
         with open(file_path, mode="w", newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerow(table['header'])
-            writer.writerows(table['rows'])
+            writer = csv.writer(file) # для записи данных в файл.
+            writer.writerow(table['header']) # записывает список заголовков как первую строку.
+            writer.writerows(table['rows']) # записывает все строки таблицы.
     except Exception as e:
         raise ValueError(f"Ошибка при сохранении файла {file_path}: {e}")
         
