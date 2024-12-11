2edc0fa8e884538bf0d8a5623ebba20535111e59
Ivan Nikolaev <149399445+0bobux@users.noreply.github.com>
2024-12-12 00:52:19 +0300

    добавил документацию в text_module

--- table_modules/text_module.py
+++ table_modules/text_module.py
@@ -1,12 +1,19 @@
 def save_table(table, file_path):
     """
     Сохраняет таблицу в текстовый файл в удобном для чтения виде.
+
+    Args:
+        table (dict): Таблица, представленная в виде словаря с ключами:
+        file_path (str): Путь к текстовому файлу, в который будет сохранена таблица.
+
+    Raises:
+        ValueError: Если возникает ошибка при сохранении файла (например, проблемы с доступом к файлу).
     """
     try:
         with open(file_path, mode='w', encoding='utf-8') as file:
-            file.write('\t'.join(table['header']) + '\n')
+            file.write('\t'.join(table['header']) + '\n') # записываем заголовок, объединяя элементы через табуляцию
             for row in table['rows']:
-                file.write('\t'.join(row) + '\n')
+                file.write('\t'.join(row) + '\n') # записываем каждую строку таблицы
     except Exception as e:
         raise ValueError(f"Ошибка при сохранении текстового файла {file_path}: {e}")
         
