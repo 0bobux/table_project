d07a68ba2f015ec2ae6286cbb39953e7803ae4eb
Ivan Nikolaev <149399445+0bobux@users.noreply.github.com>
2024-12-12 11:35:47 +0300

    Update __init__.py

--- table_modules/__init__.py
+++ table_modules/__init__.py
@@ -1,3 +1,4 @@
+# инициализации пакета
 from .csv_module import load_table as load_csv, save_table as save_csv
 from .pickle_module import load_table as load_pickle, save_table as save_pickle
 from .text_module import save_table as save_text
@@ -6,6 +7,7 @@ from .table_operations import (
     get_values, get_value, set_values, set_value, print_table, concat, split
 )
 
+# определяет список символов (функций, классов, переменных), которые будут экспортированы из пакета при использовании from package import *
 __all__ = [
     "load_csv", "save_csv", "load_pickle", "save_pickle", "save_text",
     "get_rows_by_number", "get_rows_by_index", "get_column_types", "set_column_types",
